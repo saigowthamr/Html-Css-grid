9026f9bb445e017fbf0c43deacfd1e304c71a5d7
saigowthamr <saigowthamr@gmail.com>
2018-03-12 19:16:40 +0530

    Simple template using Css Grid

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+$('.btn-scroll').click(function(){
+ $('html,body').animate({
+     scrollTop:$('#section2').offset().top
+ },1000)
+})
+
+
+$('.features-1').click(function(){
+    $('html,body').animate({
+        scrollTop:$('#section-3').offset().top
+    },1000)
+   })
+
+
+   $('.js--abt-us').click(function(){
+    $('html,body').animate({
+        scrollTop:$('#section-4').offset().top
+    },1000)
+   })
